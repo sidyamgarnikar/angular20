91413e7d3201697e82a05c79751515b0ccaf8971
sidyamgarnikar <sidyamgarnikar@gmail.com>
2026-04-10 23:49:15 +0530

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,23 @@ export class AppComponent {
  languages: Array<string> = ["JavaScript", "TypeScript", "Python"];
  subjects: Array<string> = ["Math", "Science", "English"];
  days: Array<string>= ["Monday", "Tuesday", "Wednesday"];
- days1: Array<string>= ["Monday", "Tuesday", "Wednesday"];
+ cars: Array<string> = ["BMW", "Audi", "Tesla"];
+ foods: Array<string> = ["Pizza", "Burger", "Pasta"];
+ drinks: Array<string> = ["Tea", "Coffee", "Juice"];
+flowers: Array<string> = ["Rose", "Lily", "Tulip"]
+planets: Array<string> = ["Earth", "Mars", "Venus"];
+browsers: Array<string> = ["Chrome", "Firefox", "Edge"];
+companies: Array<string> = ["Google", "Microsoft", "Amazon"];
+hobbies: Array<string> = ["Reading", "Gaming", "Traveling"];
+movies: Array<string> = ["Inception", "Avatar", "Titanic"];
+books: Array<string> = ["Ramayana", "Mahabharata", "Gita"];
+devices: Array<string> = ["Mobile", "Laptop", "Tablet"];
+seasons: Array<string> = ["Summer", "Winter", "Monsoon"];
+
+
+
+
+
+
+
 }
