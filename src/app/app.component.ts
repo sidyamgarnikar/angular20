a6df6aac527b27cd74d89bd0bc87eac577fd4d0b
sidyamgarnikar <sidyamgarnikar@gmail.com>
2026-04-10 19:57:47 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular20';
+  title = ['angular20'];
+ fruits: Array<string> = ["apple", "banana", "mango"];
+ skills: Array<string> = ["HTML","css", "Angular", "bootstrap"];
+ names:Array<string> = ["Ram", "Shyam", "Sita"];
+ flags: boolean[] = [true, false, true];
+ colors: Array<string> = ["red", "green", "blue"];
+ cities: Array<string> = ["Mumbai", "Delhi", "Pune"];
+ data: Array<any> = ["apple", 10, true];
+ isLoggedIn: Array<boolean> = [true, false, false, true];
+ countries: Array<string> = ["India", "USA", "UK"];
+ languages: Array<string> = ["JavaScript", "TypeScript", "Python"];
+ subjects: Array<string> = ["Math", "Science", "English"];
+ days: Array<string>= ["Monday", "Tuesday", "Wednesday"];
 }
