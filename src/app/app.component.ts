f175ff486cf21c23702160e276de6cbe4d7a62f3
sidyamgarnikar <sidyamgarnikar@gmail.com>
2026-04-11 00:10:21 +0530

    jhljh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = ['angular20'];
- fruits: Array<string> = ["apple", "banana", "mango"];
- skills: Array<string> = ["HTML","css", "Angular", "bootstrap"];
- names:Array<string> = ["Ram", "Shyam", "Sita"];
- flags: boolean[] = [true, false, true];
- colors: Array<string> = ["red", "green", "blue"];
- cities: Array<string> = ["Mumbai", "Delhi", "Pune"];
- data: Array<any> = ["apple", 10, true];
- isLoggedIn: Array<boolean> = [true, false, false, true];
- countries: Array<string> = ["India", "USA", "UK"];
- languages: Array<string> = ["JavaScript", "TypeScript", "Python"];
- subjects: Array<string> = ["Math", "Science", "English"];
- days: Array<string>= ["Monday", "Tuesday", "Wednesday"];
- cars: Array<string> = ["BMW", "Audi", "Tesla"];
- foods: Array<string> = ["Pizza", "Burger", "Pasta"];
- drinks: Array<string> = ["Tea", "Coffee", "Juice"];
-flowers: Array<string> = ["Rose", "Lily", "Tulip"]
-planets: Array<string> = ["Earth", "Mars", "Venus"];
-browsers: Array<string> = ["Chrome", "Firefox", "Edge"];
-companies: Array<string> = ["Google", "Microsoft", "Amazon"];
-hobbies: Array<string> = ["Reading", "Gaming", "Traveling"];
-movies: Array<string> = ["Inception", "Avatar", "Titanic"];
-books: Array<string> = ["Ramayana", "Mahabharata", "Gita"];
-devices: Array<string> = ["Mobile", "Laptop", "Tablet"];
-seasons: Array<string> = ["Summer", "Winter", "Monsoon"];
+  fruits: Array<string> = ["apple", "banana", "mango"];
+  skills: Array<string> = ["HTML", "css", "Angular", "bootstrap"];
+  names: Array<string> = ["Ram", "Shyam", "Sita"];
+  flags: boolean[] = [true, false, true];
+  colors: Array<string> = ["red", "green", "blue"];
+  cities: Array<string> = ["Mumbai", "Delhi", "Pune"];
+  data: Array<any> = ["apple", 10, true];
+  isLoggedIn: Array<boolean> = [true, false, false, true];
+  countries: Array<string> = ["India", "USA", "UK"];
+  languages: Array<string> = ["JavaScript", "TypeScript", "Python"];
+  subjects: Array<string> = ["Math", "Science", "English"];
+  days: Array<string> = ["Monday", "Tuesday", "Wednesday"];
+  cars: Array<string> = ["BMW", "Audi", "Tesla"];
+  foods: Array<string> = ["Pizza", "Burger", "Pasta"];
+  drinks: Array<string> = ["Tea", "Coffee", "Juice"];
+  flowers: Array<string> = ["Rose", "Lily", "Tulip"]
+  planets: Array<string> = ["Earth", "Mars", "Venus"];
+  browsers: Array<string> = ["Chrome", "Firefox", "Edge"];
+  companies: Array<string> = ["Google", "Microsoft", "Amazon"];
+  hobbies: Array<string> = ["Reading", "Gaming", "Traveling"];
+  movies: Array<string> = ["Inception", "Avatar", "Titanic"];
+  books: Array<string> = ["Ramayana", "Mahabharata", "Gita"];
+  devices: Array<string> = ["Mobile", "Laptop", "Tablet"];
+  seasons: Array<string> = ["Summer", "Winter", "Monsoon"];
 
 
 
